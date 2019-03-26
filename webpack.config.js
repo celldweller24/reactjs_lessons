f6f11f8516cfb439e2a13e6667e8546d7103059d
Nikita Chubukov <nikita_chubukov@epam.com>
2019-03-26 17:03:00 +0300

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,49 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
     entry: './src/index.jsx',
-    context: path.resolve(__dirname, './'),
-    devtool: 'none',
     output: {
         path: path.resolve(__dirname, './build'),
         //publicPath: '/',
         filename: 'bundle.js'
     },
+
+    context: path.resolve(__dirname, './'),
+    devtool: NODE_ENV == 'development' ? 'module-source-map' : false,
+    watch: NODE_ENV == 'development',
+    mode: 'none',
+
     plugins: [
         new HtmlWebpackPlugin({
             title: 'ReactJS lessons',
             template: 'public/index.html'
-        })
+        }),
+        new webpack.DefinePlugin({ 
+            NODE_ENV: JSON.stringify(NODE_ENV) 
+        }),
     ],
     
+    optimization: {
+        minimizer: NODE_ENV == 'production' ? [new UglifyJsPlugin({
+            parallel: true,
+            uglifyOptions: {
+                warnings: false,
+                parse: {},
+                compress: {
+                    inline: true
+                },
+                mangle: true, // Note `mangle.properties` is `false` by default.
+                toplevel: true,
+                keep_fnames: true,
+            },
+            sourceMap: true
+        })] : false
+    },
+
     module: {
         rules: [
             {
